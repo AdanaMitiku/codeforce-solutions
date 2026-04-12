d401e7a59011c45e4858b4dc3ed6ef8d3fc1f143
AdanaMitiku <adanamitiku2@gmail.com>
2026-04-12 20:07:33 +0300

    Create 32B - Borze.py

---
+++ implementation/32B - Borze.py
@@ -0,0 +1,16 @@
+s = input()
+i = 0
+ans = ""
+
+while i < len(s):
+    if s[i] == '.':
+        ans += '0'
+        i += 1
+    else: 
+        if s[i + 1] == '.':
+            ans += '1'
+        else:
+            ans += '2'
+        i += 2
+
+print(ans)
